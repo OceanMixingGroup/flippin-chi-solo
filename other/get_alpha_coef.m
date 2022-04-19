3db787fd57d070f344483e25383e1ca312491e48
Ken Hughes <hugke729@gmail.com>
2022-04-19 09:27:03 -0700

    Correct conversion between cgs and mks units

--- other/get_alpha_coef.m
+++ other/get_alpha_coef.m
@@ -11,16 +11,15 @@ function alpha_coef = get_alpha_coef(cS)
     rho = 1024; % Unit is kg/m^3
     Gs = 1.0;
     Ts = cS(1); % Unit is seconds
-    Ss = cS(4)/1e3;  % ** V m^2/N.
+    Ss = cS(4)/1e3;  % **
     alpha_inv = 2*sqrt(2)*rho*Gs*Ts*Ss;
     alpha_coef = 1/alpha_inv;
 
-    % ** Historically, header files for OSU shear calibration coefficients and
-    % profiling spped have used centimeter-gram-second (cgs) units: the shear
-    % calibration coefficient is in V/(dyne/cm^2). However, it is recorded in the
-    % header file as ~0.25 rather than 0.25x10^-4. The 10^-4 factor is in the
-    % Chameleon processing code.
+    % ** Header files for OSU shear calibration coefficients use
+    % centimeter-gram-second (cgs) units, so the shear calibration coefficient
+    % is in V/(dyne/cm^2). However, it is recorded in the header file as ~0.25
+    % rather than 0.25x10^-4. The 10^-4 factor is in the Chameleon processing code.
     %
     % Here we are using meter-kilogram-second (mks) units, so we divide the
-    % header value by 10^-3 instead of 10^-4.
+    % header value by 10^-3 to get a coefficient in V m^2/N.
 end
