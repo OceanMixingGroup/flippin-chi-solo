9fbc0450344ffeaada068c3fb7fbc1e7d99ee2d4
Ken Hughes <hugke729@gmail.com>
2025-02-11 05:15:39 +1300

    Update correct_Jan_1_2019_dates_within_sat_file.m

--- sat/correct_Jan_1_2019_dates_within_sat_file.m
+++ sat/correct_Jan_1_2019_dates_within_sat_file.m
@@ -14,9 +14,10 @@ function correct_Jan_1_2019_dates_within_sat_file(sat_filename)
 
     % Method:
     % 1. Search line by line for the affected casts, which look like
-    % '+ffffffff????2a5c.......' where ????2a5c is the datestring, part
-    % of which is fixed by the date, and part is variable with the
-    % slight time variability
+    % '+ffffffff????2a5c.......' % for an upcast and
+    % '+ffff0000ffff0000????2a5c' for a surface file
+    % where ????2a5c is the datestring, part of which is fixed by
+    % the date, and part is variable with the slight time variability
     % 2. Find the next line in the file of the form
     % '!dive  102        999 06FEB2025 00:43:50'
     % 3. Use that time (dive end) minus ~40 minutes, to get approx
@@ -27,10 +28,11 @@ function correct_Jan_1_2019_dates_within_sat_file(sat_filename)
     line_no = 1;
     for line_no = 1:length(all_lines)
         curr_line = char(all_lines(line_no));
-        if length(curr_line) < 17
+        if length(curr_line) < 25
             continue
         end
-        if strcmp(curr_line([1:9, 14:17]), '+ffffffff2a5c')
+        if (strcmp(curr_line([1:9, 14:17]), '+ffffffff2a5c') | ...
+            strcmp(curr_line([1:17, 22:25]), '+ffff0000ffff00002a5c'))
             % !dive ... line should be 60 lines later, but just search
             % over next 100 to be sure
             % new_t = curr_line(10:17);  % Set existing value initially
@@ -42,15 +44,20 @@ function correct_Jan_1_2019_dates_within_sat_file(sat_filename)
                     % Make conversion to ????2a5c form
                     new_t = posixtime(datetime(new_t)) - 40*60;
                     new_t = lower(dec2hex(new_t));
-                    new_t = [new_t(7:8) new_t(5:6) new_t(3:4) new_t(1:2)]
+                    new_t = [new_t(7:8) new_t(5:6) new_t(3:4) new_t(1:2)];
                     break
                 end
             end
             % Make replacement for earlier line
-            curr_line(1:17) = ['+ffffffff' new_t];
+            if strcmp(curr_line(2:9), 'ffffffff')
+                curr_line(1:17) = ['+ffffffff' new_t];
+            elseif strcmp(curr_line(2:17), 'ffff0000ffff0000')
+                curr_line(1:25) = ['+ffff0000ffff0000' new_t];
+            end
             all_lines(line_no) = string(curr_line);
         end
     end
     % Overwrite sat_filename with now-corrected time strings
     writelines(all_lines, sat_filename)
+    pause(1)
 end
