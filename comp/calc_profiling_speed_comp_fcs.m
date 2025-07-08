b964d74e5c260043c93a21692a20dcb91dde1ac1
Ken Hughes <hugke729@gmail.com>
2025-07-09 09:50:14 +1200

    Deal with new block issue

--- comp/calc_profiling_speed_comp_fcs.m
+++ comp/calc_profiling_speed_comp_fcs.m
@@ -8,6 +8,13 @@ function Vavg = calc_profiling_speed_comp_fcs(Vavg, head)
 
     Delta_VP = [Delta_VP(1); Delta_VP]; % Extrapolate backward for first value
 
+    if length(Delta_VP) > 256
+        % Deal with case where new file (buffer?) is created after 256 blocks have
+        % been used up.
+        % Mostly for unit 4004 in late March onward
+        Delta_VP(257) = Delta_VP(256);
+    end
+
     dt = 1/head.primary_sample_rate;
     Vavg.Wspd = Delta_VP/(head.Nseg*dt);
 end
